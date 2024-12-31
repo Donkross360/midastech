937d3b3b6ccc11f8a653c7a68fcd0909a037edc8
Donkross360 <mart2great@yahoo.com>
2024-12-31 12:02:46 +0100

    Added: static html

---
+++ src/types/global.d.ts
@@ -0,0 +1,15 @@
+interface Window {
+    LoadSnipcart: any;
+    SnipcartSettings: {
+      protocol: string;
+      domain: string;
+      version: string;
+      currency: string;
+      modalStyle: string;
+      publicApiKey: string;
+      templatesUrl: string;
+      addProductBehavior: string;
+      timeoutDuration: number;
+      loadStrategy: string;
+    };
+  }
